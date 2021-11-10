ac11ad40da52b5b25bf14857709bbbf90c00f3bf
Rghifari <93661771+Rghifari@users.noreply.github.com>
2021-11-10 22:18:02 +0700

    Perulangan bertingkat (nested) for.py

---
+++ Perulangan bertingkat (nested) for.py
@@ -0,0 +1,13 @@
+print(' ')
+print('Perulangan bertingkat (nested) for')
+print(list(range(10)))
+print(list(range(1, 11)))
+print(list(range(2, 12)))
+print(list(range(3, 13)))
+print(list(range(4, 14)))
+print(list(range(5, 15)))
+print(list(range(6, 16)))
+print(list(range(7, 17)))
+print(list(range(8, 18)))
+print(list(range(9, 19)))
+

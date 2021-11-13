78671a7da368beb3a34b4bbc29b2419d9e7fd952
Rghifari <93661771+Rghifari@users.noreply.github.com>
2021-11-13 21:52:16 +0700

    Update Perulangan bertingkat (nested) for.py

--- Perulangan bertingkat (nested) for.py
+++ Perulangan bertingkat (nested) for.py
@@ -1,13 +1,49 @@
-print(' ')
-print('Perulangan bertingkat (nested) for')
-print(list(range(10)))
-print(list(range(1, 11)))
-print(list(range(2, 12)))
-print(list(range(3, 13)))
-print(list(range(4, 14)))
-print(list(range(5, 15)))
-print(list(range(6, 16)))
-print(list(range(7, 17)))
-print(list(range(8, 18)))
-print(list(range(9, 19)))
+s = ''
+for i in range (10):
+    s += str(i) +'   '
+print (s)
 
+s = ''
+for i in range (1, 11):
+    s += str(i) +'   '
+print (s)
+
+s = ''
+for i in range (2, 12):
+    s += str(i) +'   '
+print (s)
+
+s = ''
+for i in range (3, 13):
+    s += str(i) +'   '
+print (s)
+
+s = ''
+for i in range (4, 14):
+    s += str(i) +'   '
+print (s)
+
+s = ''
+for i in range (5, 15):
+    s += str(i) +'   '
+print (s)
+
+s = ''
+for i in range (6, 16):
+    s += str(i) +'   '
+print (s)
+
+s = ''
+for i in range (7, 17):
+    s += str(i) +'   '
+print (s)
+
+s = ''
+for i in range (8, 18):
+    s += str(i) +'   '
+print (s)
+
+s = ''
+for i in range (9, 19):
+    s += str(i) +'   '
+print (s)

6edd712ae23fa252a82afc78e72fdd4d54c9e0fa
Rghifari <93661771+Rghifari@users.noreply.github.com>
2021-11-13 21:54:55 +0700

    Bilangan acak 0.5 .py

---
+++ Bilangan acak 0.5 .py
@@ -0,0 +1,19 @@
+print('==== Bilangan Acak yang lebih kecil dari 0.5 ====')
+print(' ')
+import random
+N = int(input("Masukan nilai N : "))
+a = 0
+for x in range(N):
+    a += 1
+    b = random.uniform(.0,.5)
+    print('Data ke:',a,'==>',b)
+print('Selesai')
+print(' ')
+jawab = 'lanjutkan'
+hitung = 0
+while jawab == "lanjutkan":
+    hitung += 1
+    jawab = input('Ingin Mengulang ? (Y/N) : ')
+    if jawab == "Lanjutkan":
+        break
+print('Total perulangan : ' + str (hitung))

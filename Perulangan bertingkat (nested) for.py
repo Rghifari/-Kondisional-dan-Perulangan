b2e1b71ee48dcfac709db7e7f29f8162d3aae0a6
Rghifari <93661771+Rghifari@users.noreply.github.com>
2021-11-15 19:42:58 +0700

    Perulangan bertingkat (nested) for.py

--- Perulangan bertingkat (nested) for.py
+++ Perulangan bertingkat (nested) for.py
@@ -1,50 +1,49 @@
 s = ''
 for i in range (10):
-    s += str(i) +'   '
+    s += str(i) +'\t   '
 print (s)
 
 s = ''
 for i in range (1, 11):
-    s += str(i) +'   '
+    s += str(i) +'\t   '
 print (s)
 
 s = ''
 for i in range (2, 12):
-    s += str(i) +'   '
+    s += str(i) +'\t   '
 print (s)
 
 s = ''
 for i in range (3, 13):
-    s += str(i) +'   '
+    s += str(i) +'\t   '
 print (s)
 
 s = ''
 for i in range (4, 14):
-    s += str(i) +'   '
+    s += str(i) +'\t   '
 print (s)
 
 s = ''
 for i in range (5, 15):
-    s += str(i) +'   '
+    s += str(i) +'\t   '
 print (s)
 
 s = ''
 for i in range (6, 16):
-    s += str(i) +'   '
+    s += str(i) +'\t   '
 print (s)
 
 s = ''
 for i in range (7, 17):
-    s += str(i) +'   '
+    s += str(i) +'\t   '
 print (s)
 
 s = ''
 for i in range (8, 18):
-    s += str(i) +'   '
+    s += str(i) +'\t   '
 print (s)
 
 s = ''
 for i in range (9, 19):
-    s += str(i) +'   '
-print (s)
-
+    s += str(i) +'\t   '
+print(s)
